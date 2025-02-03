7e3b731eeec0d5bec481d21ded94b5547c90f623
Irfan Mahardika <irfanmhdk141@gmail.com>
2025-02-03 15:47:23 +0700

    lass

--- layout/header.php
+++ layout/header.php
@@ -53,7 +53,7 @@ include 'config/app.php'
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="assets-template/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
